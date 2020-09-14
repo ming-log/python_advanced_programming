b4d1db61490e183ae087aa8caabd7b09f8facc98
luoming <736704198@qq.com>
2020-09-14 23:10:52 +0800

    Initial commit

---
+++ 01 import 导入模块.py
@@ -0,0 +1,14 @@
+# __author__:"Ming Luo"
+# date:2020/9/14
+import aa
+aa.test_age()  # >>>  -----test_age-----
+# 现在修改aa模块中的内容重复，导入模块
+import aa
+aa.test_age()  # >>>  -----test_age-----
+# 由于python不会重复导入模块，故修改的内容没有生效
+from imp import reload
+reload(aa)
+aa.test_age()  # >>>  -----test_age12sadsads32-----
+
+
+
---
+++ aa.py
@@ -0,0 +1,9 @@
+# __author__:"Ming Luo"
+# date:2020/9/14
+
+age = 100
+_age = 12313
+
+def test_age():
+    print("-----test_age12sadsads32-----")
+
